c296d0c10afc30e4c74cbcd53fe47f216948175c
Mary Jack <maryjackinthebox@gmail.com>
2018-11-14 20:59:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  helper_method :cart
-  
+ def index
+    self.add_to_cart(params[:product])
+    render '/products/index'
+  end
    def cart
     session[:cart] ||= []
+  end
+   def add_to_cart(item)
+    if item && !item.strip.empty? 
+      self.cart << item.strip
+    end
   end
   
 end
